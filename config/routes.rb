13c629f884f1f9464243b549ba4f8f666e712afc
BILLEMAZ Maxime <maxime.billemaz@ynov.com>
2017-10-17 13:41:54 +0200

    Header + page d'accueil

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
+
   #mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
-
+  localized do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'homepage#index'
-
-  post '/listings/contact', to: 'listings#contact', as: 'listing_contact'
-
+    root to: 'homepage#index'
+    devise_for :users
+    post '/listings/contact', to: 'listings#contact', as: 'listing_contact'
+  end
   #apipie
 end
